43c7dc8db610e27c25ed727e795aff8ae3ea5e1a
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2020-01-28 23:07:42 +0200

    Add redux-persistor

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
-import store from './store'
+import {store, persistor} from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
-import {createStore, applyMiddleware
+import {createStore
+//, applyMiddleware
 //  , compose
 } from 'redux'; 
 // import thunk from 'redux-thunk';
 // import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {appState} from './reducers';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, appState);
+
 const store = createStore(
-  appState, 
+  persistedReducer, 
   // applyMiddleware(logger, thunk)
   // composeEnhancers(applyMiddleware(thunk))
 );  
 
+const persistor = persistStore(store);
+
+export {store, persistor};
 export default store;
