3aaeeb00f59634782dc45f79cb69a3d34131f65a
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2020-01-30 20:45:59 +0200

    Updates

---
+++ precache-manifest.bf28c70fca5120405c09033a97845e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20327856a7f7f4fbbfcda14bd1efa099",
+    "url": "/test-spa/index.html"
+  },
+  {
+    "revision": "ac95a46a66f5d88c019b",
+    "url": "/test-spa/static/css/main.a97ceeec.chunk.css"
+  },
+  {
+    "revision": "44915c952060b5d93377",
+    "url": "/test-spa/static/js/2.7fbc6947.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/test-spa/static/js/2.7fbc6947.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac95a46a66f5d88c019b",
+    "url": "/test-spa/static/js/main.379dffdb.chunk.js"
+  },
+  {
+    "revision": "19ec82b3489677a5fc0f",
+    "url": "/test-spa/static/js/runtime-main.bbff1614.js"
+  }
+]);
