d7c7167b5a6df218d8566cbe321db26caf95301e
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2020-01-30 14:57:29 +0200

    Fix update student action

--- src/store/actions.js
+++ src/store/actions.js
@@ -11,10 +11,8 @@ import {
   ADD_STUDENTS_TO_COURSE,
   DELETE_STUDENT_FROM_COURSE,
   DELETE_COURSE_FROM_STUDENT,
-} from '../common/constants';
+} from '../constants';
 import {createAction} from 'redux-actions';
-// import coursesJSON from './courses.json';
-// import studentsJSON from './students.json';
 
 export const addNewCourse = createAction(ADD_NEW_COURSE, courseData => ({courseData}));
 export const updateCourse = createAction(UPDATE_COURSE, courseData => ({courseData}));
@@ -24,32 +22,7 @@ export const addNewStudent = createAction(ADD_NEW_STUDENT, studentData => ({stud
 export const updateStudent = createAction(UPDATE_STUDENT, studentData => ({studentData}));
 export const deleteStudent = createAction(DELETE_STUDENT, studentId => ({studentId}));
 export const changeStudentsNumberOnPage = createAction(CHANGE_STUDENTS_NUMBER_ON_PAGE, rowsOnPage => ({rowsOnPage}));
-
 export const addCourseToStudent = createAction(ADD_COURSES_TO_STUDENT);
 export const addStudentToCourse = createAction(ADD_STUDENTS_TO_COURSE);
 export const deleteStudentFromCourse = createAction(DELETE_STUDENT_FROM_COURSE);
 export const deleteCourseFromStudent = createAction(DELETE_COURSE_FROM_STUDENT);
-
-
-// export const initApp = () => {
-//   return (dispatch, getState) => {
-//     // thunk initApp
-
-//     // get menu data
-
-//     // send all init data to store  
-//     dispatch(getInitData({
-//       courses: {
-//         lastID: 'C10',
-//         rowsOnPage: 50,
-//         data: [1,2,3],
-//       },
-//       students: {
-//         lastID: 'S100',
-//         rowsOnPage: 10,
-//         data: [1,2,3],
-//       },
-//     }));
-
-//   };
-// };
